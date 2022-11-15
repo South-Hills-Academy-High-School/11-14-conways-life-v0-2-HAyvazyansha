9f4efdb86a5e48bc86190aac1a2aa50b43c00d22
HAyvazyansha <113388054+HAyvazyansha@users.noreply.github.com>
2022-11-15 14:07:33 -0800

    counting without edge

--- main.ts
+++ main.ts
@@ -51,36 +51,20 @@ function drawGrid () {
     }
     cursor.left = cursorX
     cursor.top = cursorY
-    neighborCountSprite.right = cursorX
-    neighborCountSprite.bottom = cursorY
+    neighborCountSprite.left = cursorX
+    neighborCountSprite.top = cursorY
     neighborCountSprite.setText(convertToText(countNeighbors(cursorGridRow, cursorGridCol)))
 }
 function countNeighbors (currentRow: number, currentCol: number) {
     NeighboCount = 0
-    if (currentRow == 0 && currentCol == 0) {
-        NeighboCount += grid[11][15]
-    } else if (currentRow == 0) {
-        NeighboCount += grid[11][currentCol - 1]
-    } else {
-        NeighboCount += grid[currentRow - 1][currentCol - 1]
-    }
-    if (currentRow == 0) {
-        NeighboCount += grid[11][currentCol]
-    } else {
-        NeighboCount += grid[currentRow - 1][currentCol - 0]
-    }
-    if (currentRow == 0 && currentCol == 15) {
-        NeighboCount += grid[11][0]
-    } else if (currentRow == 0) {
-        NeighboCount += grid[11][currentCol + 1]
-    } else {
-        NeighboCount += grid[currentRow - 1][currentCol + 1]
-    }
-    if (currentRow == 0) {
-        NeighboCount += grid[currentRow][0]
-    } else {
-        NeighboCount += grid[currentRow - 0][currentCol + 1]
-    }
+    NeighboCount += grid[currentRow - 1][currentCol - 1]
+    NeighboCount += grid[currentRow - 1][currentCol - 0]
+    NeighboCount += grid[currentRow - 1][currentCol + 1]
+    NeighboCount += grid[currentRow - 0][currentCol + 1]
+    NeighboCount += grid[currentRow + 1][currentCol + 1]
+    NeighboCount += grid[currentRow + 1][currentCol + 0]
+    NeighboCount += grid[currentRow + 1][currentCol - 1]
+    NeighboCount += grid[currentRow + 0][currentCol - 1]
     return NeighboCount
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -119,10 +103,10 @@ cursor = sprites.create(img`
     3 . . . . . . . . 3 
     3 3 3 3 . . 3 3 3 3 
     `, SpriteKind.newCursor)
-cursorGridCol = 0
-cursorGridRow = 0
-cursorX = 0
-cursorY = 0
+cursorGridCol = 7
+cursorGridRow = 5
+cursorX = 70
+cursorY = 50
 cursor.z = 10
 neighborCountSprite = textsprite.create("")
 cursor.z = 10
